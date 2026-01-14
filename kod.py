4108857eed5be6caf9a874f9832356e9bb1aabbb
michalmistrzmaster <michal.mikolajczyk04@gmail.com>
2026-01-14 11:40:31 +0100

    01-14

--- kod.py
+++ kod.py
@@ -5,16 +5,9 @@ import matplotlib.pyplot as plt
 def main():
 
     #parametry rownania
-    t = 10
     c = 1.0
     k = 25
     omega = 10
-
-    #warunki poczatkowe
-    position0 = 0
-    velocity0 = 0
-    #zmienne parametry
-    krok = np.array([0.5,0.25,0.1,0.05])
 
     #rozwiazujemy rownanie dla wielu F0 z zakresu [0,1000]
     #tworzenie wektora F ze zmienna gestoscia w kolejnych przedzialach
@@ -22,24 +15,46 @@ def main():
     F2 = np.linspace(1, 30, 50)
     F3 = np.linspace(30, 100, 20)
     F4 = np.linspace(100, 1000, 10)
-    
     F = np.concatenate((F1, F2, F3, F4))
 
-    #scipy.integrate.solve_ivp(method='RK45', max_step=1e-4)
-
-    #E = np.max(x_num - x_ref)
-
-    np.set_printoptions(suppress=True)
+    #warunki poczatkowe
+    position0 = 0
+    velocity0 = 0
     
-    print(F)
-    print(krok)
-    x,dx_dt = MojRK2(t,c,k,omega,F[21],krok[3],position0,velocity0)
+    #czas symulacji
+    krok = np.array([0.5,0.25,0.1,0.05])
+    t_end = 10
+    N = int(t_end/krok[2] + 1)
+    t = np.linspace(0,t_end,N)
 
-    #print(x)
+    x_ref = scipy.integrate.solve_ivp(
+        system,
+        (0,t_end),
+        [position0,velocity0],
+        args=(c,k,F[21],omega),
+        t_eval = t,
+        method='RK45',
+        max_step=1e-4
+    )
 
-def MojRK2(t,c,k,omega,F,h,position0,velocity0):
-    N = int(t/h + 1)
-    t = np.linspace(0,t,N)
+    x1_ref = x_ref.y[0]
+    x2_ref = x_ref.y[1]
+
+    print(x1_ref)
+
+    x1_num,x2_num = MojRK4(t_end,c,k,omega,F[21],krok[2],position0,velocity0)
+
+    print(x1_num)
+
+def system(t, stan, c, k, F, omega):
+    x1, x2 = stan
+    dx1dt = f1(x2)
+    dx2dt = f2(x1,x2,t,c,k,omega,F)
+    return [dx1dt, dx2dt]
+
+def MojRK2(t_end,c,k,omega,F,h,position0,velocity0):
+    N = int(t_end/h + 1)
+    t = np.linspace(0,t_end,N)
 
     # inicjalizacja rownan
     x1 = np.empty(N)
@@ -49,23 +64,47 @@ def MojRK2(t,c,k,omega,F,h,position0,velocity0):
 
 
     for i in range(1,N):
-        fxy1 = x2[i-1]
-        fxy2 = -c*x2[i-1] - k*x1[i-1] + F*np.sin(omega*t[i-1])
-        x1E = x1[i-1] + h*fxy1
-        x2E = x2[i-1] + h*fxy2
-        fxyE1 = x2E
-        fxyE2 = -c*x2E - k*x1E + F*np.sin(omega*t[i])
-        x1[i] = x1[i-1] + h*0.5*(fxy1 + fxyE1)
-        x2[i] = x2[i-1] + h*0.5*(fxy2 + fxyE2)
-
-
-    print(np.array2string(x1, suppress_small=True))
+        k1_1 = h*f1(x2[i-1])
+        k2_1 = h*f2(x1[i-1],x2[i-1],t[i-1],c,k,omega,F)
+        k1_2 = h*f1(x2[i-1] + k2_1)
+        k2_2 = h*f2(x1[i-1] + k1_1 ,x2[i-1] + k2_1 ,t[i-1]+h,c,k,omega,F)
+        x1[i] = x1[i-1] + 0.5*(k1_1 + k1_2)
+        x2[i] = x2[i-1] + 0.5*(k2_1 + k2_2)
 
     return x1,x2
 
 
-def MojRK4(t,c,k,omega,F,h):
-    return 0
+def MojRK4(t_end,c,k,omega,F,h,position0,velocity0):
+    N = int(t_end/h + 1)
+    t = np.linspace(0,t_end,N)
+
+    # inicjalizacja rownan
+    x1 = np.empty(N)
+    x2 = np.empty(N)
+    x1[0] = position0
+    x2[0] = velocity0
+
+
+    for i in range(1,N):
+        k1_1 = h*f1(x2[i-1])
+        k2_1 = h*f2(x1[i-1],x2[i-1],t[i-1],c,k,omega,F)
+        k1_2 = h*f1(x2[i-1] + 0.5*k2_1)
+        k2_2 = h*f2(x1[i-1] + 0.5*k1_1 ,x2[i-1] + 0.5*k2_1 ,t[i-1]+0.5*h,c,k,omega,F)
+        k1_3 = h*f1(x2[i-1] + 0.5*k2_2)
+        k2_3 = h*f2(x1[i-1] + 0.5*k1_2 ,x2[i-1] + 0.5*k2_2 ,t[i-1]+0.5*h,c,k,omega,F)
+        k1_4 = h*f1(x2[i-1] + k2_3)
+        k2_4 = h*f2(x1[i-1] + k1_3 ,x2[i-1] + k2_3 ,t[i-1]+h,c,k,omega,F)
+        x1[i] = x1[i-1] + (1/6)*(k1_1 + 2*k1_2 + 2*k1_3 + k1_4)
+        x2[i] = x2[i-1] + (1/6)*(k2_1 + 2*k2_2 + 2*k2_3 + k2_4)
+
+    return x1,x2
+
+def f1(x2):
+    return x2
+
+def f2(x1,x2,t,c,k,omega,F):
+    return -c*x2 - k*x1 + F*np.sin(omega*t)
+
 
 
 if __name__ == "__main__":
